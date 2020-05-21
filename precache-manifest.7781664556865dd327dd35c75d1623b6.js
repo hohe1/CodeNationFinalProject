9b2a858843943d6c53c7415c2f465f6321423ef3
hohe1 <anthonyho02015@gmail.com>
2020-05-20 17:21:38 -0700

    Updates

---
+++ precache-manifest.7781664556865dd327dd35c75d1623b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d465c6408287bbd717334b797178e9",
+    "url": "/CodeNationFinalProject/index.html"
+  },
+  {
+    "revision": "773a660d214e725043b8",
+    "url": "/CodeNationFinalProject/static/css/main.bb7f90d7.chunk.css"
+  },
+  {
+    "revision": "6942e36d8c57025f874b",
+    "url": "/CodeNationFinalProject/static/js/2.f4701478.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CodeNationFinalProject/static/js/2.f4701478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773a660d214e725043b8",
+    "url": "/CodeNationFinalProject/static/js/main.5bf54bdb.chunk.js"
+  },
+  {
+    "revision": "a4287e9f1ad032a4fd6c",
+    "url": "/CodeNationFinalProject/static/js/runtime-main.d9bec0e9.js"
+  }
+]);
